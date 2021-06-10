6b5276b12039db10a30147e9c557ef5ae47d6823
Mauro Nunes <maurao.dev@gmail.com>
2021-06-09 22:20:18 -0300

    First commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  1: const User(
+    id: '1',
+    name: "Maria",
+    email: 'maria@alunos.com.br',
+    avatarUrl:'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_960_720.png'),
+  )
+};
